120a1f8eaab400fe6d2b9a3b0b9d49b9ff5c265f
Zaheer <zaheer.mughal1965@gmail.com>
2019-07-06 13:49:08 +0500

    Ready to upload

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,12 +10,16 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-
+                        <span class="label label-default">
+                        *demo Email:    <strong>shahzaib@gmail.com</strong><br>
+                        *demo passwrod: <strong>shahzaib1234</strong>
+                      </span> <br><br><br>
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus
+                                  value="shahzaib@gmail.com">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -29,7 +33,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" value="shahzaib1234">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
